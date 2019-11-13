f4c82b10d101a084cefecebd7297da49a37dac4f
Destiny Ross <destinyross.dev@gmail.com>
2019-11-12 20:52:36 -0500

    initialized hello doge react app

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{ margin: "0 0 10vh 0" }}>BORK</h1>
+        <img
+          className="App-logo"
+          src="https://www.stickpng.com/assets/thumbs/5845e755fb0b0755fa99d7f3.png"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
